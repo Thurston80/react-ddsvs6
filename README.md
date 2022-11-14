7791c542b847d1c7cf8dbb1dbdf9db29e3e3e03c
Thurston80 <william_story80@yahoo.com>
2022-11-14 10:14:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ddsvs6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ddsvs6)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from './components/header/header-component'
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
